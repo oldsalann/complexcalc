ca453d1220006193eba2b230bf9123794b51f58b
Nicholas Olds <oldsn@Nicholass-MacBook-Pro.local>
2018-10-04 18:17:02 -0700

    simple tests work

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,52 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    var left : Int
+    var right: Int
+    
+    init() {
+        self.left = 0
+        self.right = 0
+    }
+    
+    func add(lhs: Int, rhs: Int) -> Int {
+        return lhs + rhs
+    }
+    
+    func add(_ nums : [Int]) -> Int {
+        var ret = 0;
+        for i in nums {
+            ret = ret + i
+        }
+        return ret
+    }
+    
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return lhs - rhs
+    }
+    
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return lhs * rhs
+    }
+    
+    func multiply(_ nums : [Int]) -> Int {
+        var ret = 0;
+        for i in nums {
+            ret = ret * i
+        }
+        return ret
+    }
+    
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return lhs / rhs
+    }
+    
+    func count(_ nums : [Int]) -> Int {
+        return nums.count
+    }
+    
+    func avg(_ nums: [Int]) -> Int {
+        let total = self.add(nums)
+        return total / nums.count
+    }
 }
